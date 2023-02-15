b2d82f390cad06dd298733abb332915da8798ec4
Aruzhan Ismagulova <aruzhanism@mail.ru>
2023-02-16 03:38:54 +0600

    first commit

---
+++ cmd/api/users.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"text/template"
+
+	"todolist.net/internal/data"
+	"todolist.net/internal/validator"
+)
+
+// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
+// return a plain-text placeholder response.
+func (app *application) homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	http.ServeFile(w, r, "templates/Untitled-2.html")
+
+}
+
+func (app *application) welcomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Copy the data from the request body into a new User struct. Notice also that we
+	// set the Activated field to false, which isn't strictly necessary because the
+	// Activated field will have the zero-value of false by default. But setting this
+	// explicitly helps to make our intentions clear to anyone reading the code.
+	user := &data.User{
+		Name:      r.FormValue("txt"),
+		Email:     r.FormValue("email"),
+		Activated: false,
+	}
+	tpl, err := template.ParseFiles("templates/welcome.html")
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	dat := r.FormValue("txt")
+
+	tpl.ExecuteTemplate(w, "welcome.html", dat)
+
+	// Use the Password.Set() method to generate and store the hashed and plaintext
+	// passwords.
+	err = user.Password.Set(r.FormValue("pswd"))
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	v := validator.New()
+	// Validate the user struct and return the error messages to the client if any of
+	// the checks fail.
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Insert the user data into the database.
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		// If we get a ErrDuplicateEmail error, use the v.AddError() method to manually
+		// add a message to the validator instance, and then call our
+		// failedValidationResponse() helper.
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
