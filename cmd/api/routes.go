5e3bb1e037f83e69521b6fc9b7ce9f40adb7b5d3
Aruzhan Ismagulova <aruzhanism@mail.ru>
2023-02-19 22:17:40 +0600

    re-start

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,12 +11,13 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	router.ServeFiles("/templates/*filepath", http.Dir("./templates/"))
-	
+
 	router.HandlerFunc(http.MethodGet, "/", app.homeHandler)
 
 	router.HandlerFunc(http.MethodPost, "/welcome", app.welcomeHandler)
-	router.HandlerFunc(http.MethodPost, "/tasks", app.createAuthenticationTokenHandler)
+	router.HandlerFunc(http.MethodPost, "/myacc", app.createAuthenticationTokenHandler)
 	router.HandlerFunc(http.MethodGet, "/activated/:token", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPost, "/myacc/tasks", app.createAuthenticationTokenHandler)
 
 	// Return the httprouter instance.
 	return router
