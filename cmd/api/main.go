fb1f9d825131f5f163ec7f4d54fba7741e990309
Aruzhan Ismagulova <aruzhanism@mail.ru>
2023-02-24 14:42:13 +0600

    heroku

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -51,7 +51,7 @@ func main() {
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	db, err := sql.Open("postgres", "postgres://admin:gudron@hostname:5432/todolistsslmode=require")
 
 	if err != nil {
 		logger.Fatal(err)
