9bcb062ce6a1e2574df622e8f1e3feaebce6ff4a
Aruzhan Ismagulova <aruzhanism@mail.ru>
2023-02-24 14:13:08 +0600

    heroku

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -53,7 +53,7 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request)
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.logError(r, err)
+	app.logger.Printf("Server error: %v", err)
 	message := "the server encountered a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
