6b8df44cfec8402dd441d39088e6b8b38a8ac74c
Aruzhan Ismagulova <aruzhanism@mail.ru>
2023-02-24 15:35:21 +0600

    heroku

--- migrations/000003_create_todo_list_table.up.sql
+++ migrations/000003_create_todo_list_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS lists (
 ID bigserial PRIMARY KEY,
 user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE,
-title text NOT NULL
+title text NOT NULL,
+created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
